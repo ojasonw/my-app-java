5bd9036b15001f17274fc3726302d2f8071a8191
gabriel <sene300@gmail.com>
2026-04-10 16:08:40 -0300

    feat: novo endpoint add user para agendamento

---
+++ src/main/java/br/com/joga_together/dto/scheduling/SchedulingsByGroupIdResponseDto.java
@@ -0,0 +1,18 @@
+package br.com.joga_together.dto.scheduling;
+
+import br.com.joga_together.dto.user.GroupResponseToSchedulingDto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+public record SchedulingResponseDto(
+        UUID id,
+        String name,
+        LocalDate date,
+        LocalTime time,
+        List<UserResponseToSchedulingDto>users,
+        GroupResponseToSchedulingDto group
+) {
+}
