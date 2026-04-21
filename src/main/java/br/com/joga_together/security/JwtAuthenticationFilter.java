b31813302be808bbceeda09e6c96e9ab39bc3ae3
gabriel <sene300@gmail.com>
2026-04-20 22:02:50 -0300

    feat: add new return user object to login

--- src/main/java/br/com/joga_together/security/JwtAuthenticationFilter.java
+++ src/main/java/br/com/joga_together/security/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.RequestContextFilter;
 
 import java.io.IOException;
 
